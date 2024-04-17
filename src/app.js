c59a16c0af151ccea29e765930186e0fa43776eb
igaraeva <161602447+igaraeva@users.noreply.github.com>
2024-04-17 13:15:36 +0100

    Added API integration to search engine

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
+function displayWeather(response) {
+  let currentLocation = document.querySelector("#current-location");
+  let currentTemperature = document.querySelector("#current-temperature-value");
+  let temperature = Math.round(response.data.temperature.current);
+  currentLocation.innerHTML = response.data.city;
+  currentTemperature.innerHTML = temperature;
+}
+
+function searchLocation(city) {
+  let apiKey = "51f9883450a0f1682ta31o8477291fb1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
 function searchCityWeather(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
-  let currentLocation = document.querySelector("#current-location");
-  currentLocation.innerHTML = searchFormInput.value;
+
+  searchLocation(searchFormInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchCityWeather);
+
+searchLocation("London");
