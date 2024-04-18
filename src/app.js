5b39e86da9de4bb7043813c0cf0f1998fb5d2a15
igaraeva <161602447+igaraeva@users.noreply.github.com>
2024-04-18 12:11:06 +0100

    Injected timestamp

--- src/app.js
+++ src/app.js
@@ -6,13 +6,33 @@ function displayWeather(response) {
   let weatherDescription = document.querySelector("#weather-description");
   let humidity = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#wind-speed");
+  let currentTime = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
 
   currentLocation.innerHTML = response.data.city;
   currentTemperature.innerHTML = Math.round(temperature);
   weatherDescription.innerHTML = response.data.condition.description;
-  humidity.innerHTML = response.data.temperature.humidity;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeed.innerHTML = `${response.data.wind.speed} m/sec`;
+  currentTime.innerHTML = formatDate(date);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let hours = date.getHours().toString().padStart(2, "0");
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = weekDays[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
 function searchLocation(city) {
   let apiKey = "51f9883450a0f1682ta31o8477291fb1";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}}&key=${apiKey}&units=metric`;
